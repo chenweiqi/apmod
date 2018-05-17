f96940fe8fd614cdd36e684bc0bc19d3911c93ca
surenkid <surenkids@gmail.com>
2018-05-17 14:35:57 +0800

    BUG - Resolve compile error on Linux

--- mod_concatx/mod_concatx.c
+++ mod_concatx/mod_concatx.c
@@ -85,7 +85,11 @@ static int set_file_type = -1;				//-1:default limit;0:no limit;>0:user limit
 static char **sep_file_type = NULL;
 
 static const char s_szDefalteFilterName[]="DEFLATE";  // gzip; deflate
+#if defined(WIN32)
 extern  __declspec(dllimport) module **ap_loaded_modules;
+#else
+extern module **ap_loaded_modules;
+#endif
 static int mod_defalte_loaded = 0;
 
 static const char *
